52ad6bd635fe3efb8d5259cb96c87c351ad49551
Jone Mose <jonemose@student.21-school.ru>
2026-06-17 21:50:06 +0700

    feat: добавить VacancySource interface

--- internal/parser/interface.go
+++ internal/parser/interface.go
@@ -1,1 +1,11 @@
 package parser
+
+import (
+	"context"
+
+	"github.com/EgorTur/CareerHunter/internal/domain"
+)
+
+type VacancySource interface {
+	Fetch(ctx context.Context, keywords []string) ([]domain.Vacancy, error)
+}
